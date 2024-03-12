074d011ac05169d838e1728f833dcbc3ec847687
Emily <157151877+emlyuwu@users.noreply.github.com>
2024-03-12 18:06:49 +0000

    2nd commit, continuation of creation

--- game/main.py
+++ game/main.py
@@ -2,41 +2,39 @@ import random
 
 
 class city():
-    def __init__(self, pop, police, entropy):
+    def __init__(self, pop, police, entropy, inventory):
         self.pop = pop
         self.police = police
         # Police
         self.entropy = entropy
         # population impacts price, police impacts police, entropy impacts random encounters.
+        self.inventory = inventory
 
     def buy(self, item):
         self.item = item
 
     def sell(self, item):
         self.item = item
-    def store(self, inventory):
-        self.inventory = inventory
-        print("NOT FINISHED")
     def storegenerate(self):
         if self in citylist:
             print(self)
             # print("success")
             # print(self.pop, self.police, self.entropy)
             ## the former 2 lines are for testing
-            self.store.inventory = ""
             iter = self.pop
+            self.inventory = ""
             while iter > 0:
-                self.store.inventory = self.store.inventory.append(random.getrandbits(1))
+                self.inventory += str(random.getrandbits(1))
                 iter -= 1
                 #append inventory with 1 or 0 for inventory. 101 equals true false true
         else:
             print(self)
             print("failure")
 
-bug = city(pop=3,police=0, entropy=1)
-bug2 = city(pop=5,police=.3, entropy=.6)
+bug = city(pop=3,police=0, entropy=1, inventory="")
+bug2 = city(pop=5,police=.3, entropy=.6, inventory="")
 citylist = [bug, bug2]
-print()
+print(bug, "print bug")
 prompt = input("city name\n")
 if prompt == "bug":
     storegen = bug
@@ -44,8 +42,8 @@ elif prompt ==  "bug2":
     storegen = bug2
 else:
     storegen = f'invalid city name "{prompt}"'
+print(bug.inventory)
 city.storegenerate(storegen)
-print(city.store.inventory)
 
 class stuff():
     def __init__(self, id, name, price, rarity, variance):
